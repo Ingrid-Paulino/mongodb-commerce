ffafc970bf1bc0f76c28d5f2709ccc7da723ea4f
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-04-06 15:59:31 -0300

    C14 - add req 11

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,11 +2,9 @@ db.produtos
   .find(
     {
       valoresNutricionais: {
-        $elemMatch: { tipo: "calorias", percentual: { $gte: 30, $lte: 40 } },
+        $elemMatch: { tipo: "proteínas", percentual: { $gte: 30, $lte: 40 } },
       },
     },
     { nome: 1, _id: 0 },
   )
   .pretty();
-
-  db.produtos.find({ valoresNutricionais: { $elemMatch: { tipo: "calorias", percentual: { $gte: 30, $lte: 40 } } } }, { nome: 1, _id: 0 }).pretty();
