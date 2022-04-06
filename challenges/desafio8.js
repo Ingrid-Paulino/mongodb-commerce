ef823f5c54a3a574fa49b447096da8e051fec4ea
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-04-06 16:41:20 -0300

    C15 - add req 8 com sucesso

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,4 +1,5 @@
 // Delete os lanches que tenham menos de 50 curtidas e retorne o nome dos lanches que restaram no banco.
 
-db.produtos.remove({ curtidas: { $lt: 50 } });
-db.produtos.find().pretty();
+db.produtos.deleteMany({ curtidas: { $lt: 50 } });
+
+db.produtos.find({}, { nome: 1, _id: 0 }).pretty();
