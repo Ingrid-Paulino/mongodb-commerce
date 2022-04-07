0f534c46aece3e57ddfed03c55829a59e4869db6
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-04-07 12:25:49 -0300

    C22 - add req 19

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,3 +1,5 @@
+// Retorne a quantidade total de produtos em uma nova coleção chamada resumoProdutos.
+
 db.resumoProdutos.insertOne({
   franquia: "McDonalds",
   totalProdutos: db.produtos.countDocuments({}),
