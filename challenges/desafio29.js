1e5f52ec07d7c434cd7e20a9cdf9e1f365de549e
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-04-08 11:35:07 -0300

    C32 - add requisito 30

--- challenges/desafio29.js
+++ challenges/desafio29.js
@@ -1,3 +1,5 @@
+// Renomeie o campo descricao para descricaoSite em todos os documentos.
+
 db.produtos.updateMany({}, { $rename: { descricao: "descricaoSite" } });
 
 db.produtos.find({}, { nome: 1, descricao: 1, descricaoSite: 1, _id: 0 });
---
+++ challenges/desafio30.js
@@ -0,0 +1,5 @@
+// Remova o campo curtidas do item Big Mac.
+
+db.produtos.updateMany({ nome: "Big Mac" }, { $unset: { curtidas: "" } });
+
+db.produtos.find({}, { nome: 1, curtidas: 1, _id: 0 });
