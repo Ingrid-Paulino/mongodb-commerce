87722602b74d758f2cd1b3dde0eb26767ef16036
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-04-08 10:35:33 -0300

    C27 - add requisito 25

---
+++ challenges/desafio23.js
@@ -0,0 +1,9 @@
+// Insira os elementos combo e tasty no array tags de todos os sanduíches e aproveite para deixar os elementos em ordem alfabética ascendente.
+
+db.produtos.updateMany({}, { $push: { 
+  tags: { 
+    $each: ["combo", "tasty"],
+    $sort: { tags: 1 },
+} } });
+
+db.produtos.find({}, { nome: 1, tags: 1, _id: 0 }).sort({ tags: 1 });
---
+++ challenges/desafio24.js
@@ -0,0 +1,5 @@
+// Ordene em todos os documentos os elementos do array valoresNutricionais pelo campo percentual de forma descendente.
+
+db.produtos.updateMany({}, { valoresNutricionais: { $each: [], $sort: { percentual: 1 } } });
+
+db.produtos.find({}, { nome: 1, valoresNutricionais: 1, _id: 0 });
