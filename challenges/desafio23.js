e16655464504cdb22981f6339e4262524814b7cc
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-04-08 11:07:48 -0300

    C30 - add requisito 23 e 24

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -3,7 +3,7 @@
 db.produtos.updateMany({}, { $push: { 
   tags: { 
     $each: ["combo", "tasty"],
-    $sort: { tags: 1 },
+    $sort: 1,
 } } });
 
-db.produtos.find({}, { nome: 1, tags: 1, _id: 0 }).sort({ tags: 1 });
+db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
